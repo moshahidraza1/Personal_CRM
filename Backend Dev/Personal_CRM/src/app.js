b60103e5e112609de23c8b6bc144e634d853b42b
moshahidraza1 <codinghandle@gmail.com>
2025-02-03 17:20:44 +0530

    Added helmet for protection against web vulnerabilities

--- Backend Dev/Personal_CRM/src/app.js
+++ Backend Dev/Personal_CRM/src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import healthRouter from "./routes/health.routes.js";
 import testRouter from "./routes/test.routes.js";
 import errorHandler from "./middlewares/errorHandler.middleware.js";
+import helmet from "helmet";
 import morgan from "morgan";
 
 const app = express();
@@ -11,6 +12,7 @@ const app = express();
 // });
 app.use(errorHandler); // for handling errors
 app.use(morgan('dev')); // Logs requests like "GET /api/health 200 12ms"
+app.use(helmet()); // for security
 app.use("/api/v1/health", healthRouter);
 
 app.use("/api/v1/test", testRouter);
