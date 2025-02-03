3660c87096079eaed4c43e740441799f88e3404e
moshahidraza1 <codinghandle@gmail.com>
2025-02-03 17:15:56 +0530

    added query validationResult from exprerr-validator to sanitize input, prevent XSS attack and to respond user about input error

--- Backend Dev/Personal_CRM/src/routes/test.routes.js
+++ Backend Dev/Personal_CRM/src/routes/test.routes.js
@@ -1,8 +1,16 @@
 import {Router} from "express";
 import testRouter from "../controllers/test.controller.js";
+import { query, validationResult } from "express-validator";
 
 const router = Router();
 
-router.route("/").get(testRouter);
+router.route("/",
+    [query("name").trim().escape() // sanitize input
+    ], (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+    }).get(testRouter);
 
 export default router;
