33064f16bacb7eb35d9bf01fd0947c3cf65be988
moshahidraza1 <codinghandle@gmail.com>
2025-02-07 20:18:38 +0530

    Added prisma and created postres connection

--- Backend Dev/Personal_CRM/src/index.js
+++ Backend Dev/Personal_CRM/src/index.js
@@ -1,9 +1,6 @@
 import dotenv from "dotenv";
 import {app} from "./app.js";
 dotenv.config();
-// dotenv.config({
-//     path: "./config.env"
-// });
 
 app.listen(process.env.PORT,()=>{
     console.log(`Server is running on port: ${process.env.PORT}`);
